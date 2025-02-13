1bf030ae8c3037e906b6e96124f675c0f0e2cd56
Tacatoco <tacauzumaki837@gmail.com>
2025-02-13 13:28:00 +0100

    Add files via upload

--- Colecciones/src/Usuario.java
+++ Colecciones/src/Usuario.java
@@ -1,13 +1,25 @@
-import java.util.Map;
+package COLECCIONES;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class Usuario {
     private String nombre;
     private int edad;
+    private String DNI;
 
-    public Usuario(String nombre, int edad) {
+    public Usuario(String nombre, int edad, String DNI) {
         this.nombre = nombre;
         this.edad = edad;
+        this.DNI = DNI;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public int getEdad() {
@@ -18,12 +30,19 @@ public class Usuario {
         this.edad = edad;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getDNI() {
+        return DNI;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
     }
+
+
+
+
+
+
+
 
 }
