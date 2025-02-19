3504d14401d79017b0cafb0946bfea3bb203a18b
Tacatoco <tacauzumaki837@gmail.com>
2025-02-19 19:16:11 +0100

    ACTUALIZADO COLECCIONES

---
+++ Colecciones/src/Ampliacioncarrito.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Ampliacioncarrito {
+    public static void main(String[] args) {
+        Set<String> carrito = new HashSet<>();
+
+
+
+
+    }
+
+
+
+
+}
