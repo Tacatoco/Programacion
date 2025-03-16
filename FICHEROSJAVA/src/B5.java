0396aee65c0f7b0ee0a3aed5fa3626e1c564d6d9
Tacatoco <tacauzumaki837@gmail.com>
2025-03-16 23:28:55 +0100

    Update B5.java

--- FICHEROSJAVA/src/B5.java
+++ FICHEROSJAVA/src/B5.java
@@ -1,4 +1,3 @@
-package EjerciciosFicheros;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -9,47 +8,61 @@ import java.util.Map;
 public class B5 {
     public static void main(String[] args) throws IOException {
         ArrayList<String> listapalabras = new ArrayList<>();
-        Map<Character, List<String>> lista=new HashMap<>();
-        for(char letra='a';letra<='z';letra++){
-            lista.put(letra,new ArrayList<>());
+        Map<Character, List<String>> lista = new HashMap<>();
+        for (char letra = 'a'; letra <= 'z'; letra++) {
+            lista.put(letra, new ArrayList<>());
         }
-        File carpeta = new File("../Diccionario");
+        File carpeta = new File("./Diccionario");
 
-        if (!carpeta.exists()){
+        if (!carpeta.exists()) {
             carpeta.mkdir();
-        }
-        else System.out.println("LA CARPETA YA ESTA CREADA");
-
+        } else System.out.println("LA CARPETA YA ESTA CREADA");
 
         String rutadic = "Documentos/diccionario.txt";
-        try (BufferedReader archivos = new BufferedReader(new FileReader(rutadic))){
-        ArrayList<String> linearchivo = new ArrayList<>();
-        String linea;
-        while((linea = archivos.readLine()) != null){
-        linea.trim();
-        linearchivo.add(linea);
-        }
-
-
-
-        }
-
-
-
-        for (int i = 0; i < abecedario.size(); i++) {
-            String nombrearchivo = abecedario.get(i) + ".txt";
-            File archivo = new File(carpeta+"/"+nombrearchivo);
-            if (archivo.createNewFile()){
-                System.out.println("Archivo creado");
+        try (BufferedReader archivos = new BufferedReader(new FileReader(rutadic))) {
+            ArrayList<String> linearchivo = new ArrayList<>();
+            String linea;
+            while ((linea = archivos.readLine()) != null) {
+                linea = linea.trim();
+                linearchivo.add(linea);
             }
         }
 
+        for (int i = 0; i < lista.size(); i++) {
+            for (char letra1 = 'a'; letra1 <= 'z'; letra1++) {
+                String nombrearchivo = letra1 + ".txt";
+                File archivo = new File(carpeta + "/" + nombrearchivo);
+                if (archivo.createNewFile()) {
+                    System.out.println(nombrearchivo + " ha sido creado");
+                }
+            }
+        }
 
+        String rutadiccionario = "C:\\Users\\Usuario\\IdeaProjects\\FICHEROSJAVA\\Documentos\\diccionario.txt";
 
-
-
-
-
-
+        try (BufferedReader lecturapalabras = new BufferedReader(new FileReader(rutadiccionario))) {
+            ArrayList<String> palabros = new ArrayList<>();
+            String linea;
+            while ((linea = lecturapalabras.readLine()) != null) {
+                linea = linea.trim();
+                palabros.add(linea);
+            }
+            for (String palabro : palabros) {
+                char caracter = palabro.charAt(0);
+                if (lista.containsKey(caracter)) {
+                    lista.get(caracter).add(palabro);
+                    System.out.println(palabro);
+                }
+            }
+        }
+        for (char letra1 = 'a'; letra1 <= 'z'; letra1++) {
+            String archivodestino = "Diccionario\\" + letra1 + ".txt";
+            try (BufferedWriter palabraescrita = new BufferedWriter(new FileWriter(archivodestino))) {
+                for (String palabra : lista.get(letra1)) {
+                    palabraescrita.write(palabra);
+                    palabraescrita.newLine();
+                }
+            }
+        }
     }
 }
